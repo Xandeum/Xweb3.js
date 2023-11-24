4aa8e81f90a90de6f58255d6842b9c40cf1e1fe2
Callum McIntyre <callum.mcintyre@solana.com>
2023-11-24 18:28:23 +0000

    refactor(experimental): export types required for get-account-info-test.ts

--- packages/rpc-transport/src/index.ts
+++ packages/rpc-transport/src/index.ts
@@ -1,4 +1,5 @@
 export * from './json-rpc';
+export type { SolanaJsonRpcErrorCode } from './json-rpc-errors';
 export * from './json-rpc-subscription';
 export type { IRpcApi, IRpcSubscriptionsApi, IRpcWebSocketTransport, PendingRpcSubscription, Rpc, RpcRequest, RpcSubscriptions } from './json-rpc-types';
 export * from './transports/http/http-transport';
