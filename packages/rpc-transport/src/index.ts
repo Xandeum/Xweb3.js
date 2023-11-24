7a1a84b82f2aa1a1dfafed9dc95ea3b1c7c63ede
Callum McIntyre <callum.mcintyre@solana.com>
2023-11-24 17:24:59 +0000

    refactor(experimental): export types required for rpc-request-coalescer.ts

--- packages/rpc-transport/src/index.ts
+++ packages/rpc-transport/src/index.ts
@@ -2,4 +2,5 @@ export * from './json-rpc';
 export * from './json-rpc-subscription';
 export type { Rpc, RpcSubscriptions } from './json-rpc-types';
 export * from './transports/http/http-transport';
+export type { IRpcTransport } from './transports/transport-types';
 export * from './transports/websocket/websocket-transport';
