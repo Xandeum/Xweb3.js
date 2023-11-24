9df0e659671e7082db83497a0af9ef54481d6b38
Callum McIntyre <callum.mcintyre@solana.com>
2023-11-24 17:15:09 +0000

    refactor(experimental): export types required for airdrop-confirmer.ts

--- packages/rpc-core/src/rpc-subscriptions/index.ts
+++ packages/rpc-core/src/rpc-subscriptions/index.ts
@@ -52,8 +52,8 @@ export function createSolanaRpcSubscriptionsApi(
                 ...rawParams: Parameters<
                     (SolanaRpcSubscriptions &
                         SolanaRpcSubscriptionsUnstable)[TNotificationName] extends CallableFunction
-                        ? (SolanaRpcSubscriptions & SolanaRpcSubscriptionsUnstable)[TNotificationName]
-                        : never
+                    ? (SolanaRpcSubscriptions & SolanaRpcSubscriptionsUnstable)[TNotificationName]
+                    : never
                 >
             ): RpcSubscription<
                 ReturnType<(SolanaRpcSubscriptions & SolanaRpcSubscriptionsUnstable)[TNotificationName]>
@@ -84,3 +84,5 @@ export function createSolanaRpcSubscriptionsApi_UNSTABLE(
         SolanaRpcSubscriptions & SolanaRpcSubscriptionsUnstable
     >;
 }
+
+export type { SignatureNotificationsApi };
